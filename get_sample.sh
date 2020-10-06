25b7f6834856e0c6bf4fe72cf6fb4aeab09231a0
Shunsuke Kimura <kimushun1101@gmail.com>
2020-10-06 23:38:50 +0900

    wget samples

---
+++ get_sample.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd `dirname $0`
+cd src
+wget -O publisher_member_function.cpp https://raw.githubusercontent.com/ros2/examples/master/rclcpp/topics/minimal_publisher/member_function.cpp
+wget -O subscriber_member_function.cpp https://raw.githubusercontent.com/ros2/examples/master/rclcpp/topics/minimal_subscriber/member_function.cpp
