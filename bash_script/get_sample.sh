06ec0e89ddc0c20b1a671db5b3eb00afb079b976
Shunsuke Kimura <kimushun1101@gmail.com>
2020-10-09 10:01:52 +0900

    move get_sample.sh to bash_script

--- bash_script/get_sample.sh
+++ bash_script/get_sample.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd `dirname $0`
-cd src
+cd ../src
 wget -O publisher_member_function.cpp https://raw.githubusercontent.com/ros2/examples/master/rclcpp/topics/minimal_publisher/member_function.cpp
 wget -O subscriber_member_function.cpp https://raw.githubusercontent.com/ros2/examples/master/rclcpp/topics/minimal_subscriber/member_function.cpp
