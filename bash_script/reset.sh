8d1ea25b5f407ebdfa36a9286c026eafcfbbdcee
Shunsuke Kimura <kimushun1101@gmail.com>
2020-10-10 23:42:59 +0900

    input 2 line feed

--- bash_script/reset.sh
+++ bash_script/reset.sh
@@ -4,8 +4,10 @@ cd ..
 
 git reset --hard
 rm -f src/file_name.cpp
+
 DIR='../include/package_name'
 if [ -d $DIR ];then
   mv $DIR ../include/ros_package_temp
 fi
+
 mv ../../package_name ../../ros_package_temp
